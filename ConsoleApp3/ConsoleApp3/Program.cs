0de16b2c53be1b01c0570728626d0364bd26f763
Dmitry10D <79482393+Dmitry10D@users.noreply.github.com>
2021-03-04 11:32:16 +0300

    Lesson3 Tasks

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            //initialization of array
+            int [,] array = new int [5,5];
+            for(int i=0; i< array.GetLength(0); i++)
+            {
+                for(int j=0; j< array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(0,9); 
+                }
+            }
+            //Вывод диагональных значений в столбик )
+            Console.WriteLine("Диагональные элементы в столбик :)");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == j) Console.WriteLine(array[i, j]); 
+                }
+            }
+            //Вывод значений матрицы
+            Console.WriteLine("Матрица");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                   Console.Write(array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Sea Battle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sea_Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] thesea = new char[10,10];
+            //char[] seablock = {0xB1};
+            //U+2592 0xB1 море
+            //U+2588 0xDB палуба
+            
+            Console.OutputEncoding = Encoding.UTF8;
+            var ch = (char)0x2592;
+            for (int i = 0; i < thesea.GetLength(0); i++)
+            {
+                for(int j = 0; j < thesea.GetLength(1); j++)
+                {
+                    thesea[i, j] = ch;
+                    //thesea[i, j] = Encoding.ASCII.GetChars(01);
+                }
+            }
+            for (int i = 0; i < thesea.GetLength(0); i++)
+            {
+                for (int j = 0; j < thesea.GetLength(1); j++)
+                {
+                    Console.WriteLine(thesea[i, j]);
+                }
+            }
+        }
+    }
+}
